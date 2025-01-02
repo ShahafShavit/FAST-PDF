5c77f9fcb80b728ea8bfcffba8ecc45163caac55
ShahafShavit <shavitshahaf@gmail.com>
2025-01-02 16:42:26 +0200

    Release script to disable accidental debug mode deployment

---
+++ EnsureDebugModeOff.ps1
@@ -0,0 +1,21 @@
+﻿# EnsureDebugModeOff.ps1
+param (
+    [string]$FilePath
+)
+
+if (-Not (Test-Path $FilePath)) {
+    Write-Output "File not found: $FilePath"
+    exit 1
+}
+
+$json = Get-Content $FilePath | ConvertFrom-Json
+$json.Debug = $false
+$json.SavePath = $null
+$json | ConvertTo-Json -Depth 10 | Set-Content $FilePath
+$check = Get-Content $FilePath | ConvertFrom-Json
+if ($json.Debug) {
+    Write-Output "Error saving changes to json file."
+    exit 1
+}
+
+Write-Output "DebugMode set to false in $FilePath"
