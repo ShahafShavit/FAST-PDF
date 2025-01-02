421b2d4d78893ed6c0cbefe91ecda5ea977f7c18
ShahafShavit <shavitshahaf@gmail.com>
2025-01-02 16:43:39 +0200

    disabled version check for developemt environment

--- Program.cs
+++ Program.cs
@@ -18,13 +18,19 @@ namespace Auto_UI_Test
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            /*
+            //var dr = MessageBox.Show("Check for updates?", "Update?", MessageBoxButtons.OKCancel);
+            //if (dr == DialogResult.OK)
+            //{
+#if DEBUG
+            Application.Run(new Main());
+#else
             using (var updateDialog = new UpdateDialog())
             {
                 updateDialog.ShowDialog();
             }
-            */
+            //}
             Application.Run(new Main());
+#endif
         }
     }
 }
